05361dc7e4732ee5345a193a1b7d06ac728570ec
ciceroot <ciceroot@gmail.com>
2015-07-14 11:27:59 +0200

    Registro y login

--- CamTrip/ViewController.swift
+++ CamTrip/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var txtpass: UITextField!
     
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    var respuestaPHP:NSString?;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +37,172 @@ class ViewController: UIViewController {
 
     @IBAction func login(sender: AnyObject) {
         //funcion para hacer login
+        let txtCorreo:String = txtemail.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet());
+        let txtPassword:String = txtpass.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet());
+        
+        if count(txtCorreo) == 0 {
+            let alert:UIAlertView = UIAlertView(title: "Error", message: "Todos los campos son obligatorios", delegate: nil, cancelButtonTitle: "Vale");
+            alert.show();
+        } else if count(txtPassword) == 0{
+            let alert:UIAlertView = UIAlertView(title: "Error", message: "Todos los campos son obligatorios", delegate: nil, cancelButtonTitle: "Vale");
+            alert.show();
+        } else {
+            //hacemos login
+            //1º Mostramos el loading
+            loading.hidden = false;
+            
+            //2º Url del webservice
+            let urlPHP = "http://webdisseny.es/camtrip/Camtrip.php?p=login";
+            
+            //3º Preparamos el paquete para el webservice
+            var datosEnvio:NSMutableString = "correo=";
+            datosEnvio.appendString(txtCorreo);
+            datosEnvio.appendString("&password=");
+            datosEnvio.appendString(txtPassword);
+            
+            //4º Preparamos al carter
+            var request = NSMutableURLRequest(URL: NSURL(string: urlPHP)!);
+            request.HTTPMethod = "POST";
+            
+            //5º Ponemos mochila al carterlo
+            request.HTTPBody = (datosEnvio as String).dataUsingEncoding(NSUTF8StringEncoding)
+            
+            //6º Creamos un proceso nuevo, donde a request con el paquete datosEnvio envie los datos al webservice
+            let proceso: Void = NSURLSession.sharedSession().dataTaskWithRequest(request,
+                completionHandler:{(data:NSData!,response:NSURLResponse!,error:NSError!) in
+                    
+                    
+                    if error != nil {
+                        var alert:UIAlertView = UIAlertView(title: "Error", message: "Error al conectar", delegate: nil, cancelButtonTitle: "Ok");
+                        alert.show();
+                        
+                    }else{
+                        //TODO EL CODIGO CUANDO TENGAMOS LA RESPUESTA DEL WEBSERVICE
+                        self.respuestaPHP = NSString(data: data, encoding: NSUTF8StringEncoding);
+                        
+                        //MATAR AL PROCESO ACTUAL, Y VOLVER AL PROCESO MAIN
+                        dispatch_async(dispatch_get_main_queue(), {
+                            
+                            //AQUI DENTRO ESTOY DE NUEVO EN EL PROCESO MAIN
+                            self.loading.hidden = true;
+                            self.comprobarRespuesta();
+                            
+                        });
+                    }
+                    
+                    
+                }
+                ).resume();
+  
+            
+        }
+        
         
     }
 
     
     @IBAction func registrar(sender: AnyObject) {
         
+        //funcion para hacer login
+        let txtCorreo:String = txtemail.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet());
+        let txtPassword:String = txtpass.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet());
+        
+        if count(txtCorreo) == 0 {
+            let alert:UIAlertView = UIAlertView(title: "Error", message: "Todos los campos son obligatorios", delegate: nil, cancelButtonTitle: "Vale");
+            alert.show();
+        } else if count(txtPassword) == 0{
+            let alert:UIAlertView = UIAlertView(title: "Error", message: "Todos los campos son obligatorios", delegate: nil, cancelButtonTitle: "Vale");
+            alert.show();
+        } else {
+            //hacemos login
+            //1º Mostramos el loading
+            loading.hidden = false;
+            
+            //2º Url del webservice
+            let urlPHP = "http://webdisseny.es/camtrip/Camtrip.php?p=registro";
+            
+            //3º Preparamos el paquete para el webservice
+            var datosEnvio:NSMutableString = "correo=";
+            datosEnvio.appendString(txtCorreo);
+            datosEnvio.appendString("&password=");
+            datosEnvio.appendString(txtPassword);
+            
+            //4º Preparamos al carter
+            var request = NSMutableURLRequest(URL: NSURL(string: urlPHP)!);
+            request.HTTPMethod = "POST";
+            
+            //5º Ponemos mochila al carterlo
+            request.HTTPBody = (datosEnvio as String).dataUsingEncoding(NSUTF8StringEncoding)
+            
+            //6º Creamos un proceso nuevo, donde a request con el paquete datosEnvio envie los datos al webservice
+            let proceso: Void = NSURLSession.sharedSession().dataTaskWithRequest(request,
+                completionHandler:{(data:NSData!,response:NSURLResponse!,error:NSError!) in
+                    
+                    
+                    if error != nil {
+                        var alert:UIAlertView = UIAlertView(title: "Error", message: "Error al conectar", delegate: nil, cancelButtonTitle: "Ok");
+                        alert.show();
+                        
+                    }else{
+                        //TODO EL CODIGO CUANDO TENGAMOS LA RESPUESTA DEL WEBSERVICE
+                        self.respuestaPHP = NSString(data: data, encoding: NSUTF8StringEncoding);
+                        
+                        //MATAR AL PROCESO ACTUAL, Y VOLVER AL PROCESO MAIN
+                        dispatch_async(dispatch_get_main_queue(), {
+                            
+                            //AQUI DENTRO ESTOY DE NUEVO EN EL PROCESO MAIN
+                            self.loading.hidden = true;
+                            self.comprobarRegistro();
+                            
+                        });
+                    }
+                    
+                    
+                }
+                ).resume();
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    func comprobarRespuesta(){
+        if(respuestaPHP == "-1"){
+            var alert:UIAlertView = UIAlertView(title: "Error", message: "Usuario o contraseña incorrectas, prueba con otras o registra un nuevo usuario", delegate: nil, cancelButtonTitle: "ok");
+            alert.show();
+        }else{
+            //almacenamos la id en el local storage
+            let localStorage = NSUserDefaults.standardUserDefaults();
+            localStorage.setObject(respuestaPHP, forKey: "id_usuario");
+            
+            //redireccionamos a la ventana principal, el listado
+            let viewLista = self.storyboard?.instantiateViewControllerWithIdentifier("listaViajes") as! ViajesTableViewController;
+            
+            self.navigationController?.pushViewController(viewLista, animated: true);
+            
+        }
+    }
+    func comprobarRegistro(){
+        if(respuestaPHP == "-1"){
+            var alert:UIAlertView = UIAlertView(title: "Error", message: "Usuario registrado.", delegate: nil, cancelButtonTitle: "ok");
+            alert.show();
+        }else{
+            //almacenamos la id en el local storage
+            let localStorage = NSUserDefaults.standardUserDefaults();
+            localStorage.setObject(respuestaPHP, forKey: "id_usuario");
+            
+            //redireccionamos a la ventana principal, el listado
+            let viewLista = self.storyboard?.instantiateViewControllerWithIdentifier("listaViajes") as! ViajesTableViewController;
+            
+            self.navigationController?.pushViewController(viewLista, animated: true);
+            
+        }
     }
     
 }
